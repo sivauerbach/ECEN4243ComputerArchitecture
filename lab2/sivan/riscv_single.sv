e5352dff9863e94446fbb3c9f06deff3220e03cb
sivauerbach <114685221+sivauerbach@users.noreply.github.com>
2023-03-20 13:22:15 -0500

    small changes

--- lab2/sivan/riscv_single.sv
+++ lab2/sivan/riscv_single.sv
@@ -144,7 +144,7 @@ module maindec (input  logic [6:0] op,
       // RegWrite_ImmSrc_ALUSrc_MemWrite_ResultSrc_Branch_ALUOp_Jump
       7'b0000011: controls = 12'b1_000_1_0_01_0_00_0; // lw, lh, lb
       7'b0110111: controls = 12'b1_100_1_0_00_0_10_0; //lui in ALU
-      7'b0100011: controls = 12'b0_001_1_1_00_0_00_0; // sw
+      7'b0100011: controls = 12'b0_001_1_1_00_0_00_0; // sw, sb, sh
       7'b0110011: controls = 12'b1_xxx_0_0_00_0_10_0; // R–type
       7'b1100011: controls = 12'b0_010_0_0_00_1_01_0; // B-Type
       7'b0010011: controls = 12'b1_000_1_0_00_0_10_0; // I–type ALU
